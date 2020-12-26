5d728aef783947fad3321caf263c28330ba522bd
bmonish <monishmonib@gmail.com>
2020-12-26 21:00:43 +0530

    Added Button Handlers

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 let isRunning = true;
+
+const secondsInMin = 60;
+const mins_25 = 25 * secondsInMin;
+const mins_30 = 30 * secondsInMin;
+const mins_5 = 5 * secondsInMin;
+
+let currentDuration = mins_25;
+
 const playPauseButton = document.getElementById("playPause");
 
 function playPause() {
@@ -7,4 +15,18 @@ function playPause() {
   } else {
     playPauseButton.innerHTML = "Play";
   }
+}
+
+//Button Handler
+function updateDuration(mins) {
+  if (mins == 30) {
+    currentDuration = mins_30;
+  } else if (mins == 5) {
+    currentDuration = mins_5;
+  } else {
+    currentDuration = mins_25;
+  }
+
+  timeLeft = currentDuration;
+  updateString();
 }
